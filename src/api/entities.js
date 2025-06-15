f083e7ea0394f3347914c371458317bfdd7f30c9
Gingi892 <kalisto892il@gmail.com>
2025-06-15 13:02:33 +0300

    Add src/api/entities.js - Base44 entity exports

---
+++ src/api/entities.js
@@ -0,0 +1,15 @@
+import { base44 } from './base44Client';
+
+
+export const Report = base44.entities.Report;
+
+export const Alert = base44.entities.Alert;
+
+export const NewsSource = base44.entities.NewsSource;
+
+export const Weapon = base44.entities.Weapon;
+
+
+
+// auth sdk:
+export const User = base44.auth;
