4d4578c87f613ede07ee6baa409e99beb4321a8b
Gingi892 <kalisto892il@gmail.com>
2025-06-15 13:09:46 +0300

    Add src/components/index.js - Components barrel file

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+// Component exports for easier imports
+export { default as LiveReportEngine } from './LiveReportEngine';
+export { default as NotificationManager } from './NotificationManager';
+export { default as ReportCard } from './ReportCard';
+export { default as StatusIndicator } from './StatusIndicator';
